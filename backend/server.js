776bb498d7421c0be343f766850296efe83f6c4c
govindjadhav <jgovind155@gmail.com>
2026-02-04 18:00:46 +0530

    Initial commit

---
+++ backend/server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// routes
+app.use("/api", require("./routes/testRoutes"));
+
+const PORT = 5000;
+app.listen(PORT, () => {
+    console.log(`Backend running on port ${PORT}`);
+});
